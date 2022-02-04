dd262acbcf4e2485ed5b39a68edb57f204fbdd31
nathalia <nathaliatroina@gmail.com>
2022-02-04 15:52:55 -0500

    commit tree fe025dc892370d938dc111f3e706a403a162f867

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,32 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event){
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event){
+    setLastName(event.target.value);
+  }
+
+//after you create ParenteComponent,
+//then form can became
+
+//import React from "react";(without the { userState })
+
+// function Form({
+//   firstName,
+//   lastName,
+//   handleFirstNameChange,
+//   handleLastNameChange,
+// }) {
+
+
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
